877a644ccda4a08b8954d96f19defdfe164a12a5
Sarah Razafindrazaka <sarah.razafindrazaka@epita.fr>
2016-01-20 17:12:07 +0100

    Init

---
+++ rush.h
@@ -0,0 +1,186 @@
+
+#ifndef RUSH_H_
+#define RUSH_H_
+
+#include <errno.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum
+{
+    rush_message_type_none                = 0,
+    rush_message_type_new_file            = 1,
+    rush_message_type_list_files          = 2,
+    rush_message_type_list_files_response = 3,
+    rush_message_type_get_file            = 4,
+    rush_message_type_get_file_response   = 5,
+    rush_message_type_file_available_here = 6,
+    rush_message_type_alive               = 7,
+    rush_message_type_discover            = 8,
+    rush_message_type_invalid
+} rush_message_type;
+
+typedef enum
+{
+    rush_message_version_none  = 0,
+    rush_message_version_1     = 1,
+    rush_message_version_invalid
+} rush_message_version;
+
+typedef enum
+{
+    rush_digest_type_none    = 0,
+    rush_digest_type_sha1    = 1,
+    rush_digest_type_sha256  = 2,
+    rush_digest_type_blake2b = 3,
+    rush_digest_type_invalid
+} rush_digest_type;
+
+typedef enum
+{
+    rush_address_type_none   = 0,
+    rush_address_type_v4     = AF_INET,
+    rush_address_type_v6     = AF_INET6
+} rush_address_type;
+
+#define RUSH_DIGEST_SHA1_SIZE (20)
+#define RUSH_DIGEST_SHA256_SIZE (32)
+#define RUSH_DIGEST_BLAKE2B_SIZE (64)
+#define RUSH_DIGEST_MAXIMUM_SIZE (RUSH_DIGEST_BLAKE2B_SIZE)
+
+#define RUSH_ADDRESS_TYPE_V4_SIZE (4)
+#define RUSH_ADDRESS_TYPE_V6_SIZE (16)
+
+static inline size_t rush_digest_type_to_size(rush_digest_type const type)
+{
+    size_t result = 0;
+
+    switch(type)
+    {
+    case rush_digest_type_sha1:
+        result = (RUSH_DIGEST_SHA1_SIZE);
+        break;
+    case rush_digest_type_sha256:
+        result = (RUSH_DIGEST_SHA256_SIZE);
+        break;
+    case rush_digest_type_blake2b:
+        result = (RUSH_DIGEST_BLAKE2B_SIZE);
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
+
+static inline size_t rush_address_type_to_size(rush_address_type const type)
+{
+    size_t result = 0;
+
+    switch(type)
+    {
+    case rush_address_type_v4:
+        result = (RUSH_ADDRESS_TYPE_V4_SIZE);
+        break;
+    case rush_address_type_v6:
+        result = (RUSH_ADDRESS_TYPE_V6_SIZE);
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
+
+static inline int rush_raw_buffer_to_sockaddr_storage(char const * const buffer,
+                                                      size_t const buffer_size,
+                                                      rush_address_type const type,
+                                                      struct sockaddr_storage * const storage,
+                                                      size_t * const storage_size)
+{
+    int result = 0;
+
+    if (buffer != NULL &&
+        storage != NULL &&
+        storage_size != NULL &&
+        (type == rush_address_type_v4 ||
+         type == rush_address_type_v6))
+    {
+        if (type == rush_address_type_v4)
+        {
+            struct sockaddr_in * sa = (struct sockaddr_in *) storage;
+            *storage_size = sizeof *sa;
+            storage->ss_family = AF_INET;
+            sa->sin_family = AF_INET;
+            sa->sin_port = 0;
+            memcpy(&(sa->sin_addr.s_addr), buffer, buffer_size);
+        }
+        else
+        {
+            struct sockaddr_in6 * sa = (struct sockaddr_in6 *) storage;
+            *storage_size = sizeof *sa;
+            storage->ss_family = AF_INET6;
+            sa->sin6_family = AF_INET6;
+            sa->sin6_port = 0;
+            memcpy(&(sa->sin6_addr), buffer, buffer_size);
+        }
+    }
+    else
+    {
+        result = EINVAL;
+    }
+
+    return result;
+}
+
+static inline int rush_addrinfo_to_raw_buffer(struct addrinfo const * const info,
+                                              char ** const out,
+                                              size_t * const out_size,
+                                              rush_address_type * const type)
+{
+    int result = 0;
+
+    if (info != NULL &&
+        (info->ai_family == AF_INET ||
+         info->ai_family == AF_INET6) &&
+        out != NULL &&
+        out_size != NULL &&
+        type != NULL)
+    {
+        *type = info->ai_family == AF_INET ? rush_address_type_v4 : rush_address_type_v6;
+        *out_size = rush_address_type_to_size(*type);
+        *out = malloc(*out_size);
+
+        if (*out != NULL)
+        {
+            struct sockaddr const * const sa = info->ai_addr;
+
+            if (*type == rush_address_type_v4)
+            {
+                struct sockaddr_in * sai = (struct sockaddr_in *) sa;
+                assert(sai->sin_family == AF_INET);
+                memcpy(*out, &(sai->sin_addr.s_addr), *out_size);
+            }
+            else
+            {
+                struct sockaddr_in6 * sai = (struct sockaddr_in6 *) sa;
+                assert(sai->sin6_family == AF_INET6);
+                memcpy(*out, &(sai->sin6_addr), *out_size);
+            }
+        }
+        else
+        {
+            result = ENOMEM;
+        }
+    }
+    else
+    {
+        result = EINVAL;
+    }
+
+    return result;
+}
+
+#endif /* RUSH_H_ */
