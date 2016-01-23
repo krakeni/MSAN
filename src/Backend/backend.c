27c74e5b1f299fbbbe25d806e6058604addf588d
Pippas <hamdao.y@gmail.com>
2016-01-23 23:38:43 +0100

    Fix on mcast_send

--- src/Backend/backend.c
+++ src/Backend/backend.c
@@ -28,24 +28,24 @@ typedef struct
 
 } rush_backend_config;
 
-void rush_backend_send_mcast_msg_san(uint8_t *databuf, int datalen)
+void rush_backend_send_mcast_msg_san(char *databuf, int datalen)
 {
     struct in_addr Local_interface = (struct in_addr) { 0 };
     struct sockaddr_in mcast_sock = (struct sockaddr_in) { 0 };
 
-    int socket = -1;
+    int msocket = socket(AF_INET, SOCK_DGRAM, 0);
     memset((char *) &mcast_sock, 0, sizeof(mcast_sock));
     mcast_sock.sin_family = AF_INET;
     mcast_sock.sin_addr.s_addr = inet_addr(FRONTEND_GROUP);
     mcast_sock.sin_port = htons(FE_MCAST_PORT);
 
     Local_interface.s_addr = inet_addr(LOCAL_IFACE);
-    if(setsockopt(socket, IPPROTO_IP, IP_MULTICAST_IF, (char *)&Local_interface, sizeof(Local_interface)) < 0)
+    if(setsockopt(msocket, IPPROTO_IP, IP_MULTICAST_IF, (char *)&Local_interface, sizeof(Local_interface)) < 0)
     {
         perror("Setting local interface error");
         exit(1);
     }
-    if(sendto(socket, databuf, datalen, 0, (struct sockaddr*)&mcast_sock, sizeof(mcast_sock)) < 0)
+    if(sendto(msocket, databuf, datalen, 0, (struct sockaddr*)&mcast_sock, sizeof(mcast_sock)) < 0)
     {
         perror("Sending datagram message error");
     }
@@ -763,6 +763,12 @@ int main(void)
 
     int multicast_socket = -1;
     rush_backend_bind_multicast_socket(&multicast_socket);
+
+    /* FOR TEST PURPOSE
+    char databuf[1024] = "CLAUTOUR HANDICAP INTERNATIONAL";
+    int datalen = 1024;
+    rush_backend_send_mcast_msg_san(databuf, datalen);
+    */
 
     config.watched_dir = "/tmp";
     config.watched_dir_len = strlen(config.watched_dir);
--- src/Frontend/frontend.c
+++ src/Frontend/frontend.c
@@ -34,19 +34,19 @@ void rush_frontend_send_mcast_msg_san(uint8_t *databuf, int datalen)
     struct in_addr Local_interface = (struct in_addr) { 0 };
     struct sockaddr_in mcast_sock = (struct sockaddr_in) { 0 };
 
-    int socket = -1;
+    int msocket = socket(AF_INET, SOCK_DGRAM, 0);
     memset((char *) &mcast_sock, 0, sizeof(mcast_sock));
     mcast_sock.sin_family = AF_INET;
     mcast_sock.sin_addr.s_addr = inet_addr(SAN_GROUP);
     mcast_sock.sin_port = htons(BE_MCAST_PORT);
 
     Local_interface.s_addr = inet_addr(LOCAL_IFACE);
-    if(setsockopt(socket, IPPROTO_IP, IP_MULTICAST_IF, (char *)&Local_interface, sizeof(Local_interface)) < 0)
+    if(setsockopt(msocket, IPPROTO_IP, IP_MULTICAST_IF, (char *)&Local_interface, sizeof(Local_interface)) < 0)
     {
         perror("Setting local interface error");
         exit(1);
     }
-    if(sendto(socket, databuf, datalen, 0, (struct sockaddr*)&mcast_sock, sizeof(mcast_sock)) < 0)
+    if(sendto(msocket, databuf, datalen, 0, (struct sockaddr*)&mcast_sock, sizeof(mcast_sock)) < 0)
     {
         perror("Sending datagram message error");
     }
