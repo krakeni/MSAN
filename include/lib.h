43c382f0fca2c355a51af9a57012df016cc3580e
Alexis Daviot <daviot_a@epita.fr>
2016-01-24 02:15:52 +0100

    type 8

--- include/lib.h
+++ include/lib.h
@@ -1,4 +1,25 @@
 #ifndef LIB_H_
 #define LIB_H_
 
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/inotify.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+void send_mcast_msg(char *databuf, int datalen, uint16_t port, const char* mcast_group);
+
+void send_mcast_discover(uint16_t port, const char* mcast_group);
+
 #endif /* LIB_H */
--- src/lib.c
+++ src/lib.c
@@ -1,21 +1,4 @@
 #include "../include/lib.h"
-#include <arpa/inet.h>
-#include <assert.h>
-#include <errno.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <math.h>
-#include <netdb.h>
-#include <netinet/tcp.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/epoll.h>
-#include <sys/inotify.h>
-#include <sys/socket.h>
-#include <unistd.h>
-
 
 void send_mcast_msg(char *databuf, int datalen, uint16_t port, const char* mcast_group)
 {
@@ -41,3 +24,8 @@ void send_mcast_msg(char *databuf, int datalen, uint16_t port, const char* mcast
     close(msocket);
 }
 
+void send_mcast_discover(uint16_t port, const char* mcast_group)
+{
+  char msg[2];
+  send_mcast_msg(&msg, 2, port, mcast_group);
+}
