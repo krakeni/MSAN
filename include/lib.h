b065e5fec089d42215979f5f17892ae99d0c4115
Sébastien Clautour <sebastien.clautour@epita.fr>
2016-01-24 17:27:51 +0100

    Forge messages de type 1 & 2 + lib pour les listes + struct pour les données à ajouter dans le message type 3

--- include/lib.h
+++ include/lib.h
@@ -26,4 +26,12 @@ void send_mcast_discover(uint16_t port, const char* mcast_group);
 
 int send_ucast_msg(char *address, int port, uint8_t *message, long long message_length);
 
+void send_mcast_request_list_all_files_msg(uint16_t port, const char* mcast_group);
+
+void send_ucast_request_list_all_files_msg(char *address, int port);
+
+struct BE_file_info_list *BE_file_info_list_create_node(BE_file_info element);
+
+void send_mcast_list_all_files(int nb_files, struct BE_file_info_list *infos, int port, char *address);
+
 #endif /* LIB_H */
