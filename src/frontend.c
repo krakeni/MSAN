2ee49cc043b33abae341a188a7ab56173fd876c2
Sebastien Clautour <sebastien.clautour@epita.fr>
2016-01-27 16:46:00 +0100

    remove useless fct

--- src/frontend.c
+++ src/frontend.c
@@ -21,29 +21,6 @@
 #include "../include/lib.h"
 #include "../include/handlers.h"
 
-void rush_frontend_send_mcast_msg_san(uint8_t *databuf, int datalen)
-{
-    struct in_addr Local_interface = (struct in_addr) { 0 };
-    struct sockaddr_in mcast_sock = (struct sockaddr_in) { 0 };
-
-    int msocket = socket(AF_INET, SOCK_DGRAM, 0);
-    memset((char *) &mcast_sock, 0, sizeof(mcast_sock));
-    mcast_sock.sin_family = AF_INET;
-    mcast_sock.sin_addr.s_addr = inet_addr(SAN_GROUP);
-    mcast_sock.sin_port = htons(BE_MCAST_PORT);
-
-    Local_interface.s_addr = inet_addr(LOCAL_IFACE);
-    if(setsockopt(msocket, IPPROTO_IP, IP_MULTICAST_IF, (char *)&Local_interface, sizeof(Local_interface)) < 0)
-    {
-        perror("Setting local interface error");
-        exit(1);
-    }
-    if(sendto(msocket, databuf, datalen, 0, (struct sockaddr*)&mcast_sock, sizeof(mcast_sock)) < 0)
-    {
-        perror("Sending datagram message error");
-    }
-}
-
 static int rush_frontend_watch_dir(char const * const dir,
         int * const inotify_fd,
         int * const dir_inotify_fd)
